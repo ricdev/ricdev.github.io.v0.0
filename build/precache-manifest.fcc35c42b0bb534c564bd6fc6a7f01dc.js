78698fd5d20b43f17ab547c603d3a540db0193f0
ric <ricardo.monteverde@codeandtheory.com>
2022-10-27 15:26:07 +0800

    feat Inital commit

---
+++ build/precache-manifest.fcc35c42b0bb534c564bd6fc6a7f01dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a7b667a2a03402e51f",
+    "url": "/introduction/static/css/main.5cfda940.chunk.css"
+  },
+  {
+    "revision": "e0a7b667a2a03402e51f",
+    "url": "/introduction/static/js/main.e0a7b667.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/introduction/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "65e20bc62b60a776b797",
+    "url": "/introduction/static/js/runtime~main.65e20bc6.js"
+  },
+  {
+    "revision": "0119fd5d05f19aebe7d5bddd34129f00",
+    "url": "/introduction/index.html"
+  }
+];
